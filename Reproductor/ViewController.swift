5eb20656d76e9167c044a1f6560ac0174a3e0735
Arturo Barreto Villafán <Barreto@arturos-mbp.lan>
2015-12-23 17:56:55 -0600

    Initial Commit

---
+++ Reproductor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reproductor
+//
+//  Created by Arturo Barreto Villafán on 12/23/15.
+//  Copyright © 2015 Arturo Barreto Villafán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
